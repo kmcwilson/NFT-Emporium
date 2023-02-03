c9fdc77063bef58cdc71b2b10789b62930c796d8
Katie Wilson <kmcwilson4@gmail.com>
2023-02-02 20:25:20 -0500

    added to the wishlist

--- client/src/components/HomeCard.js
+++ client/src/components/HomeCard.js
@@ -5,14 +5,16 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import seeds from ''
+import seeds from '@apollo/client';
 
-export default function ImgMediaCard(props) {
+export default function HomeCard(props){
+
+  
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         component="img"
-        alt="green iguana"
+        alt={props.nft_token}
         height="140"
         image={`/static/images/cards/${props.image}`}
       />
@@ -21,18 +23,18 @@ export default function ImgMediaCard(props) {
          {props.nft_token}
         </Typography>
         <Typography gutterBottom variant="h6" component="div">
-         {props.nft_token}
+         {props.nft_collection}
         </Typography>
         <Typography variant="body2" color="text.secondary">
          {props.price} {props.owner}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to Wish List</Button>
-        <Button size="small">Buy Now!</Button>
+        <Button size="small" variant="contained">Add to Wish List</Button>
+        <Button size="small" variant="contained">Buy Now!</Button>
       </CardActions>
     </Card>
   );
-}
+  }
 
 
--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,54 +1,29 @@
 import React from 'react';
 import '../App.css';
-import { useQuery, useMutation } from '@apollo/client';
-import Card from '../components/HomeCard';
+// import HomeCard from '../components/HomeCard';
+// import {mockNfts} from '../../../server/seeders/seed';
 
-import { QUERY_USER } from '../utils/queries';
-import { REMOVE_NFT } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { removeNftId } from '../utils/localStorage';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_NFTS, QUERY_USER } from '../utils/queries';
+// import { REMOVE_NFT } from '../utils/mutations';
+// import Auth from '../utils/auth';
+// import { removeNftId } from '../utils/localStorage';
 
 export default function Wishlist() {
-  const { loading, data } = useQuery(QUERY_USER);
-  const userData = data?.me || [];
-  const userDataLength = Object.keys(userData).length;
-  const [removeNft] = useMutation(REMOVE_NFT);
 
-  if (loading) { return <h1> Loading...</h1> }
-
-  const handleDeleteNFT = async (nftId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-    try {
-      await removeNft({ variables: { nftId } });
-      removeNftId(nftId);
-    } catch (err) {
-      console.error(err);
-      alert('Something went wrong with the function!');
-    }
-  };
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
+const { loading, data } = useQuery(QUERY_NFTS);
+const nfts =  data?.nfts|| [];
+console.log('NFT',nfts);
 
   return (
     <>
-      <h1 className='buy-sell'>My Wish List</h1>
-      <h3>
+      <h1>{loading? (<div>Loading...</div>):(<div>My Wish List</div>)}</h1>
+      <p>{nfts}</p>
+      {/* <h3>
         {userData.savedNfts.length ? `You have ${userData.savedNfts.length} ${userData.savedNfts.length === 1 ? 'NFT' : 'NFTs'} on your wish list!`
           : 'You have no items on your wish list pal, get shopping!'}
-      </h3>
-      <div className="container wish-list-cards" style={{
-        display: 'flex', justifyContent: 'space-between',
-        flexWrap: 'wrap'
-      }}>
-          <div className="card" key={nft._id}></div>
-
-
-      </div>
+      </h3> */}
     </>
   );
 
