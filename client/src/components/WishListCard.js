e7550abd7521f24f6318552e8b1f0aca9b8b1430
Katie Wilson <kmcwilson4@gmail.com>
2023-02-02 21:32:22 -0500

    added to the cards

--- client/src/components/WishListCard.js
+++ client/src/components/WishListCard.js
@@ -6,27 +6,21 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { useMutation } from '@apollo/client';
+// import { QUERY_USER } from '../utils/queries';
 import { REMOVE_NFT } from '../utils/mutations';
-import Auth from '../utils/auth';
 import { removeNftId } from '../utils/localStorage';
-import seeds from ''
+
 
 export default function WishListCard(props) {
-     const { loading, data } = useQuery(QUERY_USER);
-  const userData = data?.me || [];
-  const userDataLength = Object.keys(userData).length;
+//   const { loading, data } = useQuery(QUERY_USER);
+//   const userData = data?.me || [];
+//   const userDataLength = Object.keys(userData).length;
   const [removeNft] = useMutation(REMOVE_NFT);
 
-  if (loading) { return <h1> Loading...</h1> }
+//   if (loading) { return <h1> Loading...</h1> }
 
   const handleDeleteNFT = async (nftId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
     try {
       await removeNft({ variables: { nftId } });
       removeNftId(nftId);
@@ -35,17 +29,15 @@ export default function WishListCard(props) {
       alert('Something went wrong with the function!');
     }
   };
-  if (!userDataLength) {
-    return <h2>LOADING...</h2>;
-  }
-
+ 
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
                 component="img"
                 alt="green iguana"
-                height="140"
-                image={`/static/images/cards/${props.image}`}
+                height="150"
+                width="150"
+                image={`images/${props.image}`}
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
@@ -55,11 +47,14 @@ export default function WishListCard(props) {
                     {props.nft_token}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    {props.price} {props.owner}
+                    {props.price} 
                 </Typography>
+                <Typography variant="body2" color="text.secondary">
+                {props.owner}
+                </Typography>   
             </CardContent>
             <CardActions>
-                <Button size="small" variant="outlined" startIcon={<DeleteIcon />} onClick={() => handleDeleteNFT(props._id)}>
+                <Button size="small" variant="outlined" onClick={() => handleDeleteNFT(props._id)}>
                     Delete
                 </Button>
                 <Button size="small">Buy Now!</Button>
--- client/src/pages/Wishlist.js
+++ client/src/pages/Wishlist.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import * as React from 'react';
 import '../App.css';
-// import HomeCard from '../components/HomeCard';
-// import {mockNfts} from '../../../server/seeders/seed';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+import WishListCard from '../components/WishListCard';
 
 
 import { useQuery, useMutation } from '@apollo/client';
@@ -13,18 +15,32 @@ import { QUERY_NFTS, QUERY_USER } from '../utils/queries';
 export default function Wishlist() {
 
 const { loading, data } = useQuery(QUERY_NFTS);
-const nfts =  data?.nfts|| [];
-console.log('NFT',nfts);
+const nfts = data?.nfts || [];
+console.log('Data:',data);
 
   return (
     <>
       <h1>{loading? (<div>Loading...</div>):(<div>My Wish List</div>)}</h1>
-      <p>{nfts}</p>
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {nfts.map(x=> 
+          (<WishListCard 
+          key={x._id}
+          token={x.nft_token}
+          image= {x.image}
+          collection={x.collection}
+          price= {x.price}
+          owner={x.owner}
+          />)
+          )}
+      </Grid>
+      </Box>
       {/* <h3>
         {userData.savedNfts.length ? `You have ${userData.savedNfts.length} ${userData.savedNfts.length === 1 ? 'NFT' : 'NFTs'} on your wish list!`
           : 'You have no items on your wish list pal, get shopping!'}
       </h3> */}
     </>
+    
   );
 
   //function accepts nft's mongoDB_id value and deletes the nft from the database
