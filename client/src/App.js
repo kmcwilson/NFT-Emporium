a31d7e9e8f8866e7c8c3030d05a49bd1bd34773e
hqayumie <heelaqayumie@gmail.com>
2023-02-02 21:34:12 -0500

    nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import About from './components/pages/About';
 import ContactUs from './components/pages/ContactUs';
 import SignUp from './components/pages/SignUp';
 import BuySell from './components/pages/BuySell';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Route path='/buy-sell' component={BuySell} />
 
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ client/src/components/pages/Orders.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Orders() {
+  return <h1 className='orders'>Orders</h1>;
+}
