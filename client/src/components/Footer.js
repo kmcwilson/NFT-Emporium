6a352ff06141e73e7806ed0acb1f315ea1bc3019
hqayumie <heelaqayumie@gmail.com>
2023-02-03 16:23:19 -0500

    removed footer header

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ import './footer.css'
   const Footer = () => {
     return (
       <footer>
-        <div className='footer-menu'></div>
+      
         <div className='footer__links'>
           <a href="mailto:heelaqayumie@gmail.com"> Heela Qayumie </a>
           <a href="mailto:kmcwilson4@gmail.com">Katie Wilson</a>
