520e0e6987d7a9613f4ae69f3972ca2c7123d702
hqayumie <heelaqayumie@gmail.com>
2023-02-03 19:13:41 -0500

    added code for about page, some styling, & data.js

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import '../App.css';
+import AboutCard from '../components/About';
+import data from '../utils/data';
 
-export default function About() {
-  return <h1 className='about'>About</h1>;
+const About = () => {
+  return (
+    <div className="container about-page" style={{display:'flex', justifyContent: 'space-between',
+    flexWrap: 'wrap'}}>
+      {data.map(about =>
+        (<AboutCard
+        key={about.id}
+        title={about.title}
+        image={about.image}
+        desc={about.desc}
+        github={about.github}
+        linkedin={about.linkedIn} />))}
+        </div>
+  )
 }
+export default About
