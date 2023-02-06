2f969d7f342cbc26d294377411b84a812e8a45b1
ryanparketh <ryanpark@rocketmail.com>
2023-02-06 09:54:06 -0500

    added bio to about page just need to update my pfp

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -27,10 +27,10 @@ const data = [
 {
   id: 1,
   image: '/images/IMG1.jpg',
-  title: 'Heela Qayumie',
-  desc: 'Future full stack web developer leveraging a background in finance, communications and political science to provide unique perspectives on how end-users can interact with websites and software platforms. I have bachelor of Arts majoring in both Political Science and Communications from Carleton University. I am currently working in finance as an Investment Specialist Associate. My strengths lie in teamwork, creativity, and attention to detail. I am a naturally curious person with a passion for learning and a hard worker who does not take life too seriously. ',
-  github: 'https://github.com/hqayumie',
-  linkedIn: 'https://www.linkedin.com/in/heela-qayumie-8604b6226/'
+  title: 'Ryan Park',
+  desc: 'Full stack web developer providing unique perspectives on how end-users interact with websites and software platforms. Earned a Full Stack Web Development certificate from Carleton University. Innovative problem-solver who is passionate about developing with a focus on design and development. Strengths in creativity, teamwork, and building projects from ideation to execution.',
+  github: 'https://github.com/ryanparketh',
+  linkedIn: 'https://www.linkedin.com/in/ryanparketh/'
 }
   // {
   //   id:2,
