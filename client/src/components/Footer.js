ffbb13d531c641d2df96b49c36963fc4defae3a1
ryanparketh <ryanpark@rocketmail.com>
2023-02-06 09:28:30 -0500

    fixed basic footer issues with hover

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,9 +6,15 @@ import './footer.css'
     return (
       <footer>
       
-        <div className='footer__links'>
+        <div className='footer-heela'>
           <a href="mailto:heelaqayumie@gmail.com"> Heela Qayumie </a>
+          </div>
+
+          <div className = 'footer-katie'>
           <a href="mailto:kmcwilson4@gmail.com">Katie Wilson</a>
+          </div>
+
+        <div className = 'footer-ryan'>
           <a href="mailto:ryanpark@rocketmail.com">Ryan Park</a>
           </div>
 
